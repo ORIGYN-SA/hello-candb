7d5306422b87b5a65452a3d89b3961b18d590d18
byronbecker <byron.becker@colorado.edu>
2022-12-29 20:34:44 -0800

    0.0.0-alpha.7: Add node_compatibility to dfx.json to solve issues with declaration usage in node scripts. * Remove comment_last_line.sh scripts (no longer necessary)

--- comment_last_line.sh
+++
@@ -1,6 +0,0 @@
-# Work around for https://github.com/dfinity/sdk/issues/2240
-# comments out the last line of the index.js declaration files created during the dfx generate command
-
-for f in src/declarations/*/index.js; do \
-  sed -i '' -e '$s/^/\/\//' "$f"; \
-done
